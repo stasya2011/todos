6da59b4bc924ed8b534ef5510050ce0a6c4ef911
Nastassia_Heleisha <nastassia_heleisha@epam.com>
2023-11-22 13:26:40 +0200

    input styling

--- src/app/components/wether/index.tsx
+++ src/app/components/wether/index.tsx
@@ -1,10 +1,16 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import env from "react-dotenv";
 
 const WEATHER_API_KEY = env.WEATHER_API_KEY;
 
 const Weather = () => {
-  const [weather, setWeather] = useState<any>({});
+  const [weather, setWeather] = useState<any>({
+    name: "Vilnius",
+    text: "",
+    icon: "",
+    temp: 0,
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
   const featchData = useCallback(async (city: string) => {
     try {
       const data = await fetch(
@@ -12,31 +18,53 @@ const Weather = () => {
       );
       const res = await data.json();
 
-      setWeather(res);
+      setWeather(() => ({
+        text: res.current.condition.text,
+        temp: res.current.temp_c,
+        name: res.location.name,
+        icon: res.current.condition.icon,
+      }));
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
   }, []);
 
   useEffect(() => {
-    featchData("vilnius");
+    featchData(weather.name);
   }, []);
 
   return (
     <span className="weather">
-      {weather.current ? (
+      {weather ? (
         <>
           <ul className="weather_info">
             <li>
-              {weather.current.condition ? weather.current.condition.text : ""},{" "}
-              {weather.current.temp_c}
+              {weather ? weather.text : ""},{weather.temp}
             </li>
 
-            <li>{weather.location.name}</li>
+            <li>
+              <input
+                type="text"
+                value={weather.name}
+                ref={inputRef}
+                onBlur={() => featchData(weather.name)}
+                onChange={(e) =>
+                  setWeather((prev: any) => ({ ...prev, name: e.target.value }))
+                }
+                style={{
+                  backgroundColor: "transparent",
+                  color: "teal",
+                  border: "none",
+                  borderBottom: "1px solid teal",
+                  padding: "5px",
+                  width: "100px",
+                }}
+              />
+            </li>
           </ul>
           <div>
-            {weather.current.condition.icon ? (
-              <img src={weather.current.condition.icon} alt="Weather icon." />
+            {weather.icon ? (
+              <img src={weather.icon} alt="Weather icon." />
             ) : null}
           </div>
         </>
