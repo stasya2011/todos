44131650910723279f8dcc4c33b29809b99ab698
Nastassia_Heleisha <s.geleisha@gmail.com>
2023-11-28 11:40:52 +0200

    test commit

--- src/STORE/index.ts
+++ src/STORE/index.ts
@@ -3,6 +3,7 @@ import todosReducer from "./slices/todos";
 import postsReducer from "./slices/posts";
 
 const rootReducer = combineReducers({ todosReducer, posts: postsReducer });
+const a = "test";
 
 export const store = configureStore({
   reducer: rootReducer,
